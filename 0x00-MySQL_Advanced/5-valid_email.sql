1d346afce073789dbdda93ead819e2f237b9479a
lumanya <shaibulumanya@gmail.com>
2023-10-17 12:11:39 +0300

    update trgigger

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -6,11 +6,7 @@ CREATE TRIGGER reset_valid_email  BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
 	IF OLD.email != NEW.email THEN
-	   IF OLD.valid_email = 0 THEN
-	   	  SET NEW.valid_email = 1;
-	   ELSE
-		  SET NEW.valid_email = 0;
-	   END IF;
-	 END IF;
+	   SET NEW.valid_email = 0;
+	END IF;
 END; $$
 DELIMITER ;
