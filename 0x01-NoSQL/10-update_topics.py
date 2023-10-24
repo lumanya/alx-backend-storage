54f3b8942d02689512a61e09d3bfe5f381ee23f4
lumanya <shaibulumanya@gmail.com>
2023-10-24 12:34:40 +0300

    update document

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -8,3 +8,11 @@ mongo_collection will be the pymongo collection object
 name (string) will be the school name to update
 topics (list of strings) will be the list of topics approached in the school
 """
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    """ update topics of school documents """
+    query = {"name": name}
+    new_values = {"$set": {"topics": topics}}
+    mongo_collection.update_many(query, new_values)
