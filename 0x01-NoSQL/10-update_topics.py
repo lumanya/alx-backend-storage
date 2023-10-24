483d48ebe477d4263608ee33e59f41f37b02b895
lumanya <shaibulumanya@gmail.com>
2023-10-24 12:26:52 +0300

    edit shebang

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+""" Write a Python function that changes all topics of a school document
+based on the name:
+
+Prototype: def update_topics(mongo_collection, name, topics):
+mongo_collection will be the pymongo collection object
+name (string) will be the school name to update
+topics (list of strings) will be the list of topics approached in the school
+"""
