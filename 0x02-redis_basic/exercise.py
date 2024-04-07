fb0afd173d0454b7a98bc93245d560d4ab61a93f
lumanya <shaibulumanya@gmail.com>
2024-04-07 20:35:30 +0300

    [ADD] decorator

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -28,6 +28,18 @@ parametrize Cache.get with the correct conversion function.
 import redis
 import uuid
 from typing import Union, Callable, Optional
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """Return a callable"""
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 
 class Cache:
@@ -39,6 +51,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ The method should generate a random key (e.g. using uuid), store
         the input data in Redis using the random key and return the key.
